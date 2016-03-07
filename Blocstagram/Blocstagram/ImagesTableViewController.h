c8d280c11297349cb0279c70ff2c0dd27fc9a91c
Bonnie <aalle.allen@gmail.com>
2016-03-06 22:25:42 -0500

    Created an array of images using a UITableViewCell class, while adding number of rows and sections, also implemented table view cells

---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by King Kittenhead on 3/6/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@end
