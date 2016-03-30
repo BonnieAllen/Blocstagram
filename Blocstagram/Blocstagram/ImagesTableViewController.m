a068418962d4ee9e0b083551bc3ffbf0263d33bc
Bonnie <aalle.allen@gmail.com>
2016-03-30 11:50:54 -0400

    Sizing for images

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -224,7 +224,7 @@
 {
     Media *item = [DataSource sharedInstance].mediaItems[indexPath.row];
     
-    return [MediaTableViewCell heightForMediaItem:item width:CGRectGetWidth(self.view.frame)];
+    return [MediaTableViewCell heightForMediaItem:item width:CGRectGetWidth(self.view.frame) traitCollection:self.view.traitCollection];
 }
 
 - (void) refreshControlDidFire:(UIRefreshControl *) sender
@@ -273,6 +273,11 @@
 - (void) cell:(MediaTableViewCell *)cell didTapImageView:(UIImageView *)imageView
 {
     MediaFullScreenViewController *fullScreenVC = [[MediaFullScreenViewController alloc] initWithMedia:cell.mediaItem];
+    
+    if (self.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClassRegular) {
+        fullScreenVC.modalPresentationStyle = UIModalPresentationFormSheet;
+    }
+
     
     [self presentViewController:fullScreenVC animated:YES completion:nil];
 }
